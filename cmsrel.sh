64924678dec6e2c19b78a2018aba218941eccc5a
Tai Sakuma <tai.sakuma@gmail.com>
2014-11-30 11:49:56 +0000

    add help message, add options, separate cmsrel and cmsenv

--- cmsrel.sh
+++ cmsrel.sh
@@ -2,14 +2,52 @@
 # Tai Sakuma <sakuma@fnal.gov>
 
 ##____________________________________________________________________________||
-function cmsrel
+function usage
 {
-    pwd=$(pwd)
-    pwd=${pwd/ /\\ }
+cat << EOF
+usage: $0 options
 
-    relpwd=${pwd/$loctop/}
-    remdir="${remtop}${relpwd}/"
+ Print commands that need to be executed to create a CMSSW
+ environment. The CMSSW version, the architecture, the directory and
+ the host need to be specified by variables in this file.
 
+ The commands can be exectued, for example, as follows
+
+    $0 | sh -x
+
+OPTIONS:
+ -e print cmsenv commands to enver the CMSSW environment
+ -h display help
+EOF
+}
+
+##____________________________________________________________________________||
+to_print_cmsenv_cmmands=false
+
+while getopts "hea:" opt; do
+    case $opt in
+	h)
+	    usage
+	    exit 1
+	    ;;
+	e)
+	    to_print_cmsenv_cmmands=true
+	    ;;
+	?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -$OPTARG requires an argument." >&2
+	    exit 1
+	    ;;
+
+    esac
+done
+
+##____________________________________________________________________________||
+function print_cmsrel
+{
     command="\
 export SCRAM_ARCH=${scram_arch}; \
 mkdir -p ${remdir}; \
@@ -19,14 +57,41 @@ scramv1 project -n ${new_dir} CMSSW ${cmssw_version}; \
 
 if [ $remhos = "localhost" ]; then
     echo "${command}"
-    echo "mkdir -p ${new_dir}/src"
-    echo "echo \"\n==== to be in the environment ====\nsource ~/cmsenv.sh ${remdir}${new_dir}/src\n\""
 else
     echo "ssh -n ${remhos} '${command}'"
     echo "mkdir -p ${new_dir}/src"
-    echo "echo \"\n==== to be in the environment ====\nslogin -x ${remhos}\nsource ./cmsenv.sh ${remdir}${new_dir}/src\n\""
 fi
 
+}
+
+##____________________________________________________________________________||
+function print_cmsenv
+{
+
+if [ $remhos = "localhost" ]; then
+    echo "source ~/cmsenv.sh ${remdir}${new_dir}/src"
+else
+    echo "slogin -x ${remhos}"
+    echo "source ~/cmsenv.sh ${remdir}${new_dir}/src"
+fi
+
+}
+
+##____________________________________________________________________________||
+function cmsrel
+{
+    pwd=$(pwd)
+    pwd=${pwd/ /\\ }
+
+    relpwd=${pwd/$loctop/}
+    remdir="${remtop}${relpwd}/"
+
+    if $to_print_cmsenv_cmmands; then
+	print_cmsenv
+	return 0
+    fi
+
+    print_cmsrel
 }
 
 ##____________________________________________________________________________||
