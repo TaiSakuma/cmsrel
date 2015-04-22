4a6e05fba3fa1b101e467a5a8cc163521af2fd49
Tai Sakuma <tai.sakuma@gmail.com>
2015-04-22 09:39:53 +0200

    update example

--- examples/s150422_01_CMSSW_7_5_0_pre2.sh
+++ examples/s150422_01_CMSSW_7_5_0_pre2.sh
@@ -8,21 +8,22 @@ source ./cmsrel/cmsrel.sh
 loctop="/Users/sakuma/work"
 
 ##____________________________________________________________________________||
-cmssw_version=CMSSW_7_2_3
-new_dir=s150320_01_CMSSW_7_2_3
+cmssw_version=CMSSW_7_5_0_pre2
+new_dir=s150422_01_CMSSW_7_5_0_pre2
 
 ##____________________________________________________________________________||
 remhos=sakuma@lxplus.cern.ch
 # scram_arch=slc6_amd64_gcc472
-scram_arch=slc6_amd64_gcc481
-# scram_arch=slc6_amd64_gcc491
+# scram_arch=slc6_amd64_gcc481
+scram_arch=slc6_amd64_gcc491
 remtop="/afs/cern.ch/user/s/sakuma/work/public"
 cmsrel
 
 ##____________________________________________________________________________||
 remhos=sakuma@cmslpc-sl6.fnal.gov
 # scram_arch=slc6_amd64_gcc472
-scram_arch=slc6_amd64_gcc481
+# scram_arch=slc6_amd64_gcc481
+scram_arch=slc6_amd64_gcc491
 remtop="/uscms/home/sakuma/nobackup/work"
 # cmsrel
 
